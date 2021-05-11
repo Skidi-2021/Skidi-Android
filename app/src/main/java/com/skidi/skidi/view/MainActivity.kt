41bf1bbd7a7277359d2e4553d4b93108f9a3451b
prasetyo21 <prasetyobu21@gmail.com>
2021-05-11 16:40:36 +0700

    Structure refactor (MVVM) & add dependencies

--- app/src/main/java/com/skidi/skidi/view/MainActivity.kt
+++ app/src/main/java/com/skidi/skidi/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.skidi.skidi
+package com.skidi.skidi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skidi.skidi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
