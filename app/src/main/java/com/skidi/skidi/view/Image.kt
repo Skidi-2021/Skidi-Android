bd765fb3ccc7eb8c6d2b46e6ae02a3d830e06698
prasetyo21 <prasetyobu21@gmail.com>
2021-06-02 10:24:47 +0700

    Fix getting output from tflite model

--- app/src/main/java/com/skidi/skidi/view/Image.kt
+++ app/src/main/java/com/skidi/skidi/view/Image.kt
@@ -43,6 +43,10 @@ class Image : AppCompatActivity() {
 
         val skidiModel = Model.newInstance(applicationContext)
 
+        val fileName = "label.txt"
+        val inputString = application.assets.open(fileName).bufferedReader().use{it.readText()}
+        val label = inputString.split("\n")
+
         val savedUri = intent.getStringExtra(EXTRA_IMAGE_URL)
 //        Log.d("imageeee", savedUri.toString())
         val msg = "Photo capture succeeded: $savedUri"
@@ -65,16 +69,16 @@ class Image : AppCompatActivity() {
 
         activityImageBinding.btnUpload.setOnClickListener {
             var tensorImage = TensorImage(DataType.UINT8)
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 160, 160, 3), DataType.FLOAT32)
+            val inputFeature0 =
+                TensorBuffer.createFixedSize(intArrayOf(1, 160, 160, 3), DataType.FLOAT32)
             try {
                 val source = ImageDecoder.createSource(this.contentResolver, Uri.parse(savedUri))
                 var bitmap = ImageDecoder.decodeBitmap(source)
                 bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
 
                 val imageProcessor = ImageProcessor.Builder()
-                    .add(ResizeOp(160, 160, ResizeOp.ResizeMethod.BILINEAR))
+                    .add(ResizeOp(320, 320, ResizeOp.ResizeMethod.BILINEAR))
                     .build()
-                var tensorImage = TensorImage(DataType.UINT8)
                 tensorImage.load(bitmap)
 
                 tensorImage = imageProcessor.process(tensorImage)
@@ -83,19 +87,36 @@ class Image : AppCompatActivity() {
 // Runs model inference and gets result.
                 val outputs = skidiModel.process(inputFeature0)
                 val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+                val max = getMax(outputFeature0.floatArray)
 
 //                val output = skidiModel.process(tensorImage.tensorBuffer)
 
 //                val tfImage = TensorImage.fromBitmap(newBitmap)
 //                val output = skidiModel.process(tfImage.tensorBuffer)
-                Log.d("modelOutput", outputFeature0.toString())
+                Log.d("modelOutput", "index: " + max.toString())
+                Log.d("modelOutput", outputFeature0.floatArray[max].toString())
+                Log.d("modelOutput", "label: " + label[max] )
+                Toast.makeText(baseContext, label[max], Toast.LENGTH_LONG).show()
             } catch (e: Exception) {
                 Log.e("modelOutput", e.toString())
                 Log.e("modelB", tensorImage.buffer.toString())
                 Log.e("modelBI", inputFeature0.buffer.toString())
             }
         }
-        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+//        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun getMax(arr: FloatArray): Int {
+        var ind = 0
+        var min = 0.0f
+
+        for (i in 0..6) {
+            if (arr[i] > min) {
+                ind = i
+                min = arr[i]
+            }
+        }
+        return ind
     }
 
 //    private fun convertImageViewToBitmap(v: ImageView): Bitmap? {
